21e3f0e1ed58f14af76b402bedac48b5de600ea0
Ciara Bujanos <c.buj22@gmail.com>
2017-08-29 23:19:59 -0600

    update ipsums

--- main.js
+++ main.js
@@ -9,45 +9,45 @@ const content3 = $('.contents-container-3');
 const content4 = $('.contents-container-4');
 
 tab1.on('click', () => {
-  tab1.addClass('visible');
+  tab1.addClass('active');
   content1.removeClass('hidden');
-  tab2.removeClass('visible');
+  tab2.removeClass('active');
   content2.addClass('hidden');
-  tab3.removeClass('visible');
+  tab3.removeClass('active');
   content3.addClass('hidden');
-  tab4.removeClass('visible');
+  tab4.removeClass('active');
   content4.addClass('hidden');
 })
 
 tab2.on('click', () => {
-  tab2.addClass('visible');
+  tab2.addClass('active');
   content2.removeClass('hidden');
-  tab1.removeClass('visible');
+  tab1.removeClass('active');
   content1.addClass('hidden');
-  tab3.removeClass('visible');
+  tab3.removeClass('active');
   content3.addClass('hidden');
-  tab4.removeClass('visible');
+  tab4.removeClass('active');
   content4.addClass('hidden');
 })
 
 tab3.on('click', () => {
-  tab3.addClass('visible');
+  tab3.addClass('active');
   content3.removeClass('hidden');
-  tab1.removeClass('visible');
+  tab1.removeClass('active');
   content1.addClass('hidden');
-  tab2.removeClass('visible');
+  tab2.removeClass('active');
   content2.addClass('hidden');
-  tab4.removeClass('visible');
+  tab4.removeClass('active');
   content4.addClass('hidden');
 })
 
 tab4.on('click', () => {
-  tab4.addClass('visible');
+  tab4.addClass('active');
   content4.removeClass('hidden');
-  tab1.removeClass('visible');
+  tab1.removeClass('active');
   content1.addClass('hidden');
-  tab2.removeClass('visible');
+  tab2.removeClass('active');
   content2.addClass('hidden');
-  tab3.removeClass('visible');
+  tab3.removeClass('active');
   content3.addClass('hidden');
 })
